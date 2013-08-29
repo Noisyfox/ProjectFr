8d310fe5c0466f1894cc0001ddda3a2478947a01
YeahO_O <yzwduck@gmail.com>
2013-08-29 15:43:22 +0800

    Add test for user module

---
+++ test/mytest_config.py
@@ -0,0 +1,128 @@
+#!/usr/bin/env python
+#coding: UTF8
+
+from poster.encode import multipart_encode
+from poster.streaminghttp import register_openers
+import hashlib, urllib2, json, random, unittest
+
+register_openers()
+
+def sha1(t):
+    if isinstance(t, str):
+        return hashlib.sha1(t).hexdigest()
+    if isinstance(t, unicode):
+        return sha1(t.encode('utf8'))
+    if isinstance(t, file):
+        return sha1(t.read())
+
+class config:
+    url = r'http://192.168.56.101/Fr/rest.php'
+    hashpattern = r'[0-9a-f]{20,20}'
+    
+    images = ['img/a.jpg', 'img/b.jpg', 'img/c.jpg', 'img/d.jpg', 'img/_a.jpg', 'img/_b.jpg', 'img/_c.jpg', 'img/_d.jpg']
+    
+    users = [
+        #uid,name , pwd  ,sex,ty,avatar     , sch , reg ,sid
+        (1, 'aaa', 'aaa', 1, 1, 'img/a.jpg', 'UA', 'JS', 1),
+        (2, 'bbb', 'bbb', 2, 1, 'img/b.jpg', 'UA', 'JS', 2),
+        (3, 'ccc', 'ccc', 1, 1, 'img/c.jpg', 'UA', 'SH', 3),
+        (4, 'ddd', 'ddd', 2, 1, 'img/d.jpg', 'UB', 'SH', 4),
+        (5, 'eee', 'eee', 1, 0, 'img/a.jpg', 'UA', 'JS', 0),
+        (6, 'fff', 'fff', 0, 0, 'img/b.jpg', 'UA', 'SH', 0),
+        (7, 'ggg', 'ggg', 1, 0, 'img/c.jpg', 'UB', 'JS', 0),
+        (8, 'hhh', 'hhh', 2, 0, 'img/d.jpg', 'UB', 'SH', 0)
+    ]
+    
+    shops = [
+        #uid,sid,name,  addr,       intro,       photo,        phone
+        (1,  1,  'www', 'www road', 'www intro', 'img/_a.jpg', '111'),
+        (2,  2,  'xxx', 'xxx road', 'xxx intro', 'img/_b.jpg', '222'),
+        (3,  3,  'yyy', 'yyy road', 'yyy intro', 'img/_c.jpg', '333'),
+        (4,  4,  'zzz', 'zzz road', 'zzz intro', 'img/_d.jpg', '444')
+    ]
+    
+    foods = [
+        #uid,sid,fid,name, price,intro,  photo,      spec
+        (1,  1,  1, 'af-a',1.0,  'afi-a','img/a.jpg',1),
+        (1,  1,  2, 'af-b',1.1,  'afi-b','img/b.jpg',0),
+        (1,  1,  3, 'af-c',1.3,  'afi-c','img/c.jpg',1),
+        (2,  2,  4, 'bf-a',2.1,  'bfi-a','img/a.jpg',0),
+        (2,  2,  5, 'bf-b',2.2,  'bfi-b','img/b.jpg',0),
+        (3,  3,  6, 'cf-a',3.1,  'cfi-a','img/a.jpg',0),
+        (3,  3,  7, 'cf-b',3.2,  'cfi-b','img/b.jpg',1)
+    ]
+    
+    @staticmethod
+    def UserSession(name):
+        return sha1(name)
+    
+    @staticmethod
+    def RandStr(length = None):
+        if not length:
+            length = random.randint(5, 25)
+        s = ''
+        for i in xrange(length):
+            s += chr(random.randint(33,126))
+        return s
+    
+    @classmethod
+    def InitDb(self, uploadimage = True):
+        t = urllib2.urlopen(self.url, 'method=database.reset').read()
+        if t != '{"result":1}':
+            raise Exeption('Error: Reset database')
+            
+        if uploadimage:
+            files = {}
+            names = []
+            for i in self.images:
+                n = hashlib.sha1(i).hexdigest()
+                files[n] = open(i, 'rb')
+                names.append(n)
+            files['images'] = ','.join(names)
+            files['method'] = 'database.imageupload'
+            d, h = multipart_encode(files)
+            req = urllib2.Request(self.url, d, h)
+            r = urllib2.urlopen(req)
+            t = r.read()
+            o = json.loads(t)
+            if o['result'] != 1:
+                raise Exception('Error: Uploading images')
+        
+        s = 'BEGIN;\n'
+        for uid, name, pwd, sex, ty, avatar, sch, reg, sid in self.users:
+            s += "INSERT INTO `user` (uid,name,password,sex,type,avatar,school,region) VALUES (%d,'%s','%s',%d,%d,'%s','%s','%s');\n"%(
+                uid,name,pwd,sex,ty,sha1(open(avatar,'rb')),sch,reg)
+            s += "INSERT INTO `session` (sid,uid,expire,type) VALUES ('%s',%d,DATE_ADD(NOW(),INTERVAL 30 DAY),%d);\n"%(
+                self.UserSession(name),uid,ty);
+        s += "INSERT INTO `session` (sid,uid,expire,type) VALUES ('test',1,DATE_SUB(NOW(), INTERVAL 1 HOUR), 0);\n"
+        for uid, sid, name, addr, intro, photo, phone in self.shops:
+            s += "INSERT INTO `shop` (sid,uid,name,address,introduction,photo,phonenum) VALUES (%d,%d,'%s','%s','%s','%s','%s');\n"%(
+                sid, uid, name, addr, intro, sha1(open(photo,'rb')),phone)
+        for uid,sid,fid,name,price,intro,photo,spec in self.foods:
+            s += "INSERT INTO `food` (fid,sid,name,introduction,price,photo,special) values (%d,%d,'%s','%s',%.2f,'%s',%d)\n"%(
+                fid,sid,name,intro,price,sha1(open(photo,'rb')),spec)
+        s += 'COMMIT;'
+        #print s
+        
+        d, h = multipart_encode({'method': 'database.execute', 'sql':s})
+        req = urllib2.Request(self.url, d, h)
+        r = urllib2.urlopen(req)
+        t = r.read()
+        #print t
+        o = json.loads(t)
+        if o['result'] != 1:
+            raise Exception('Error: Adding records')
+        
+    @classmethod
+    def call(self, param):
+        param['DEBUG'] = 0
+        d, h = multipart_encode(param)
+        req = urllib2.Request(config.url, d, h)
+        r = urllib2.urlopen(req).read()
+        print r
+        print 
+        return json.loads(r)
+    
+if __name__ == '__main__':
+    config.InitDb()
+    
---
+++ test/test_User.py
@@ -0,0 +1,312 @@
+#!/usr/bin/env python
+#coding: UTF8
+
+from mytest_config import *
+
+class Test_0_Register(unittest.TestCase):
+    def setUp(self):
+        config.InitDb()
+    
+    def tearDown(self):
+        pass
+    
+    def testNormalGuest(self):
+        p = {
+            'method': 'user.register',
+            'name': 'abc',
+            'password': 'def',
+            'sex': 1,
+            'type': 0,
+            'avatar': open('img/b.jpg', 'rb'),
+            'school': 'b',
+            'region': 'b'
+        }
+        o = config.call(p)
+        self.assertEqual(o['result'], 1)
+        self.assertIn('uid', o)
+        self.assertIn('session', o)
+    
+    def testNormalOwner(self):
+        p = {
+            'method': 'user.register',
+            'name': 'def',
+            'password': 'abc',
+            'sex': 1,
+            'type': 1,
+            'avatar': open('img/b.jpg', 'rb'),
+            'school': 'b',
+            'region': 'b'
+        }
+        o = config.call(p)
+        self.assertEqual(o['result'], 1)
+        self.assertIn('uid', o)
+        self.assertIn('session', o)
+    
+    def testDuplicate(self):
+        p = {
+            'method': 'user.register',
+            'name': 'aaa',
+            'password': 'aaa',
+            'sex': 0,
+            'type': 1,
+            'avatar': open('img/a.jpg', 'rb'),
+            'school': 'a',
+            'region': 'a'
+        }
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+    
+class Test_1_Login(unittest.TestCase):
+    def setUp(self):
+        config.InitDb()
+    
+    def tearDown(self):
+        pass
+    
+    def testNormalPwd(self):
+        for uid,name,pwd,sex,ty,avatar,sch,reg,sid in config.users:
+            p = {
+                'method': 'user.login',
+                'name': name,
+                'password': pwd
+            }
+            print 'Login as %s'%(name)
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertEqual(o['uid'], uid)
+            self.assertEqual(o['sex'], sex)
+            self.assertEqual(o['type'], ty)
+            self.assertEqual(o['avatar'], sha1(open(avatar,'rb')))
+            self.assertEqual(o['school'], sch)
+            self.assertEqual(o['region'], reg)
+            if (ty == 1):
+                self.assertEqual(o['sid'], sid)
+            self.assertIn('session', o)
+    
+    def testNormalSession(self):
+        for uid,name,pwd,sex,ty,avatar,sch,reg,sid in config.users:
+            p = {
+                'method': 'user.login',
+                'uid': uid,
+                'session': config.UserSession(name)
+            }
+            print 'Login as %s'%(name)
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertEqual(o['uid'], uid)
+            self.assertEqual(o['sex'], sex)
+            self.assertEqual(o['type'], ty)
+            self.assertEqual(o['avatar'], sha1(open(avatar,'rb')))
+            self.assertEqual(o['school'], sch)
+            self.assertEqual(o['region'], reg)
+            if (ty == 1):
+                self.assertEqual(o['sid'], sid)
+            self.assertIn('session', o)
+    
+    def testWrongPwd(self):
+        p = {
+            'method': 'user.login',
+            'name': 'DoNotExists',
+            'password': '=,='
+        }
+        print 'Nothing exists'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+        p = {
+            'method': 'user.login',
+            'name': 'aaa',
+            'password': '=,='
+        }
+        print 'Wrong password'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+        p = {
+            'method': 'user.login',
+            'name': 'aaa',
+            'password': '=,='
+        }
+        print 'Wrong password'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+        p = {
+            'method': 'user.login',
+            'name': 'aaa',
+            'password': 'bbb'
+        }
+        print 'Mismatch password'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+    
+    def testWrongSession(self):
+        p = {
+            'method': 'user.login',
+            'uid': 222,
+            'session': '222'
+        }
+        print 'Nothing exists'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+        p = {
+            'method': 'user.login',
+            'uid': 1,
+            'session': 'aaa'
+        }
+        print 'Wrong session'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+        p = {
+            'method': 'user.login',
+            'uid': 1,
+            'session': config.UserSession('ccc')
+        }
+        print 'Session mismatch'
+        o = config.call(p)
+        self.assertEqual(o['result'], -1)
+        
+class Test_2_Modify(unittest.TestCase):
+    def setUp(self):
+        config.InitDb()
+    
+    def tearDown(self):
+        pass
+    
+    def te_stNormal_withoutpwd(self):
+        for uid, name, pwd, sex, ty, avatar, school, region, sid in random.sample(config.users, 3):
+            sex = random.randint(0, 2)
+            school = config.RandStr()
+            region = config.RandStr()
+            avatar = random.choice(config.images)
+            
+            p = {
+                'method': 'user.modify',
+                'uid': uid,
+                'session': config.UserSession(name),
+                'password': '',
+                'sex': sex,
+                'avatar': open(avatar, 'rb'),
+                'school': school,
+                'region': region
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertIn('session', o)
+            
+            session = o['session']
+            p = {
+                'method': 'user.login',
+                'uid': uid,
+                'session': session
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertEqual(o['sex'], sex)
+            self.assertEqual(o['type'], ty)
+            self.assertEqual(o['avatar'], sha1(open(avatar, 'rb')))
+            self.assertEqual(o['school'], school)
+            self.assertEqual(o['region'], region)
+    
+    def te_stNormal_withpwd(self):
+        for uid, name, pwd, sex, ty, avatar, school, region, sid in random.sample(config.users, 3):
+            password = config.RandStr(20)
+            sex = random.randint(0, 2)
+            school = config.RandStr()
+            region = config.RandStr()
+            avatar = random.choice(config.images)
+            
+            p = {
+                'method': 'user.modify',
+                'uid': uid,
+                'session': config.UserSession(name),
+                'password': password,
+                'sex': sex,
+                'avatar': open(avatar, 'rb'),
+                'school': school,
+                'region': region
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertIn('session', o)
+            self.assertNotEqual(o['session'], config.UserSession(name))
+            
+            newsession = o['session']
+            p = {
+                'method': 'user.login',
+                'uid': uid,
+                'session': config.UserSession(name)
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], -1)
+            
+            p = {
+                'method': 'user.login',
+                'name': name,
+                'password': pwd
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], -1)
+            
+            p = {
+                'method': 'user.login',
+                'name': name,
+                'password': password
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            
+            p = {
+                'method': 'user.login',
+                'uid': uid,
+                'session': newsession
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 1)
+            self.assertEqual(o['sex'], sex)
+            self.assertEqual(o['type'], ty)
+            self.assertEqual(o['avatar'], sha1(open(avatar, 'rb')))
+            self.assertEqual(o['school'], school)
+            self.assertEqual(o['region'], region)
+        
+    def testInvalidSession_nothing(self):
+        for uid,name,pwd,sex,ty,avatar,sch,reg,sid in random.sample(config.users, 3):
+            p = {
+                'method': 'user.modify',
+                'uid': 999,
+                'session': '=,=',
+                'password': 'new shop'
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 0)
+    
+    def testInvalidSession_mismatch(self):
+        for uid,name,pwd,sex,ty,avatar,sch,reg,sid in random.sample(config.users, 3):
+            n_name = name
+            while (n_name == name):
+                _,n_name,_,_,_,_,_,_,_ = random.choice(config.users)
+            session = config.UserSession(n_name)
+            p = {
+                'method': 'user.modify',
+                'uid': uid,
+                'session': session,
+                'sex': 0
+            }
+            o = config.call(p)
+            self.assertEqual(o['result'], 0)
+    
+    def testSessionExpire(self):
+        p = {
+            'method': 'user.modify',
+            'uid': 1,
+            'session': 'test',
+            'sex': 0
+        }
+        o = config.call(p)
+        self.assertEqual(o['result'], 0)
+    
+
+if __name__ == '__main__':
+    unittest.main()
